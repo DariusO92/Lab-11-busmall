08c0c6e6f5ba28f7ba1e383f0a6764d7d6913f3a
Darius <darius.owens.j@gmail.com>
2022-03-23 16:31:47 -0700

    added more CSS

--- apps.js
+++ apps.js
@@ -149,7 +149,7 @@ function renderImgs(){
               label: '# of Votes',
               data: productVotes,
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                'rgba(200, 85, 132, 0.2)',
                   'rgba(54, 162, 235, 0.2)',
                   'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
